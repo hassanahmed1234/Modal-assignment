7190619bdc0e47fbe0daf3bec9482d52c7a3811c
hassanahmed1234 <hafizhassanahmedqadri123@gmail.com>
2025-10-31 11:04:42 +0500

    compelete modal

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+var modal = document.getElementById('mymodal')
+var overlay = document.getElementById('overlay')
+
+
+function mymodalon() {
+    overlay.style.opacity = 1
+    modal.style.display = 'block'
+    
+   
+   
+  
+    
+}
+function mymodaloff() {
+
+    overlay.style.opacity = 0
+    modal.style.display = 'none'
+
+}
+

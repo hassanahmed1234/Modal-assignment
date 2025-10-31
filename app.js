803f858b6c689525f1ce9f68702cf86068824b40
hassanahmed1234 <hafizhassanahmedqadri123@gmail.com>
2025-10-31 11:56:21 +0500

    correct the bugs

--- app.js
+++ app.js
@@ -2,21 +2,29 @@
 
 var modal = document.getElementById('mymodal')
 var overlay = document.getElementById('overlay')
+var main = document.getElementById('main')
 
 
 function mymodalon() {
     overlay.style.opacity = 1
-    modal.style.display = 'block'
-    
-   
-   
+    modal.style.opacity = 1
+    main.style.zIndex = 0
+    modal.style.zIndex = 1
+    modal.classList.add('animate__animated','animate__zoomIn')
+
+setTimeout(()=>{
+
+    modal.classList.remove('animate__animated','animate__zoomIn')
+},600)
   
     
 }
 function mymodaloff() {
 
     overlay.style.opacity = 0
-    modal.style.display = 'none'
+    modal.style.opacity = 0
+     main.style.zIndex = 1
+    modal.style.zIndex = 0
 
 }
 
